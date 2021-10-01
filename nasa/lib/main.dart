408b051752fde609dda366d83963cced62f4acae
Beatrix <beatrixkys@gmail.com>
2021-10-01 14:56:58 +0800

    hello bro kms time

--- nasa/lib/main.dart
+++ nasa/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:nasa/pages/home.dart';
+import 'package:nasa/pages/updates.dart';
 
-void main()=>runApp(const MaterialApp(
+void main()=>runApp( MaterialApp(
   initialRoute:'/',
 
   routes:{
     '/':(context)=>Home(),
+    '/updates':(context)=>Updates(),
   }
 
 ));
--- nasa/lib/pages/home.dart
+++ nasa/lib/pages/home.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 
 
 class Home extends StatefulWidget {
-  const ({Key? key}) : super(key: key);
 
   @override
-  _State createState() => _State();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
@@ -15,22 +14,11 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: Container(
           child: Column(
-            children:[
-              items: <MarkerItem>[
-              MarkerItem(id: 1, latitude: 31.4673274, longitude: 74.2637687),
-                MarkerItem(id: 2, latitude: 31.4718461, longitude: 74.3531591),
-                ],
-                center: LatLng(31.4906504, 74.319872),
-            itemContent: (context, index) {
-              return Text("Current Item $index");
-              },
-          )
-            ]
+
+          ),
+
           ),
         ),
-      ),
-
-
     );
   }
 }
